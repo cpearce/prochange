5a3b5d2492802bbdee3b13396c995814131aa23f
Chris Pearce <cpearce@mozilla.com>
2017-05-06 22:44:48 +1200

    Print progress of Apriori.

--- apriori.py
+++ apriori.py
@@ -9,8 +9,10 @@ def containsAllSubsets(candidate, candidates):
     return True
 
 def Apriori(index, minsup):
+    print("Apriori with minsup={}".format(minsup))
     candidates = set([frozenset({i}) for i in index.items() if index.support({i}) >= minsup])
     results = list(candidates)
+    itemsetSize = 1
     while len(candidates) > 0:
         generation = set()
         for (a,b) in product(candidates, repeat=2):
@@ -18,6 +20,8 @@ def Apriori(index, minsup):
                 index.support(a | b) >= minsup and
                 containsAllSubsets(a | b, candidates)):
                 generation.add(frozenset(a | b))
+        print("Generated {} itemsets of size {}".format(len(generation), itemsetSize))
+        itemsetSize += 1
         results.extend(list(generation))
         candidates = generation
     return results
