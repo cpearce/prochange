b5a4e74dec91e0f3957294636fcd07103ca82f70
Chris Pearce <cpearce@mozilla.com>
2017-06-03 21:31:41 +1200

    Move tests to pytests.

---
+++ test_apriori.py
@@ -0,0 +1,27 @@
+from index import InvertedIndex
+from apriori import Apriori
+from item import ItemSet
+
+def test_apriori():
+    data = ("a,b,c,d,e,f\n"
+            "g,h,i,j,k,l\n"
+            "z,x\n"
+            "z,x\n"
+            "z,x,y\n"
+            "z,x,y,i\n")
+
+    expectedItemSets = {ItemSet("i"): 2 / 6,
+                        ItemSet("z"): 4 / 6,
+                        ItemSet("x"): 4 / 6,
+                        ItemSet("y"): 2 / 6,
+                        ItemSet("xz"): 4 / 6,
+                        ItemSet("yz"): 2 / 6,
+                        ItemSet("xy"): 2 / 6,
+                        ItemSet("xyz"): 2 / 6}
+
+    index = InvertedIndex()
+    index.load(data)
+    itemsets = Apriori(index, 2 / 6)
+    assert(set(expectedItemSets.keys()) == set(itemsets))
+    for itemset in itemsets:
+        assert(expectedItemSets[itemset] == index.support(itemset))
---
+++ test_index.py
@@ -0,0 +1,35 @@
+from index import InvertedIndex
+from item import Item
+
+def test_InvertedIndex():
+    data = ("a,b,c,d,e,f\n"
+            "g,h,i,j,k,l\n"
+            "z,x\n"
+            "z,x\n"
+            "z,x,y\n"
+            "z,x,y,i\n")
+    index = InvertedIndex()
+    index.load(data)
+    assert(index.support({Item("a")}) == 1 / 6)
+    assert(index.support({Item("b")}) == 1 / 6)
+    assert(index.support({Item("c")}) == 1 / 6)
+    assert(index.support({Item("d")}) == 1 / 6)
+    assert(index.support({Item("e")}) == 1 / 6)
+    assert(index.support({Item("f")}) == 1 / 6)
+    assert(index.support({Item("h")}) == 1 / 6)
+    assert(index.support({Item("i")}) == 2 / 6)
+    assert(index.support({Item("j")}) == 1 / 6)
+    assert(index.support({Item("k")}) == 1 / 6)
+    assert(index.support({Item("l")}) == 1 / 6)
+    assert(index.support({Item("z")}) == 4 / 6)
+    assert(index.support({Item("x")}) == 4 / 6)
+    assert(index.support({Item("y")}) == 2 / 6)
+
+    sup_zx = index.support({Item("z"), Item("x")})
+    assert(sup_zx == 4 / 6)
+
+    sup_zxy = index.support({Item("z"), Item("x"), Item("y")})
+    assert(sup_zxy == 2 / 6)
+
+    sup_zxyi = index.support({Item("z"), Item("x"), Item("y"), Item("i")})
+    assert(sup_zxyi == 1 / 6)
