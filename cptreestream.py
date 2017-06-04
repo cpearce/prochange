f24a71e0f4016187b2803fc7b07451e0928233cf
Chris Pearce <cpearce@mozilla.com>
2017-06-05 09:42:07 +1200

    Move CPTree streaming and test out to their own files.

---
+++ cptreestream.py
@@ -0,0 +1,49 @@
+from fptree import FPTree
+from fptree import mine_fp_tree
+from item import Item
+from collections import deque
+from fptree import sort_transaction
+from fptree import fp_growth
+
+
+def mine_cp_tree_stream(transactions, min_support, sort_interval, window_size):
+    # Yields (window_start_index, window_length, patterns)
+    tree = FPTree()
+    sliding_window = deque()
+    frequency = None
+    num_transactions = 0
+    for transaction in transactions:
+        num_transactions += 1
+        transaction = sort_transaction(map(Item, transaction), frequency)
+        tree.insert(transaction)
+        sliding_window.append(transaction)
+        if len(sliding_window) > window_size:
+            transaction = sliding_window.popleft()
+            transaction = sort_transaction(transaction, frequency)
+            tree.remove(transaction, 1)
+            assert(len(sliding_window) == window_size)
+            assert(tree.num_transactions == window_size)
+        if (num_transactions % sort_interval) == 0:
+            tree.sort()
+            frequency = tree.item_count.copy()
+        if (num_transactions % window_size) == 0:
+            if (num_transactions % sort_interval) != 0:
+                # We won't have sorted due to the previous check, so we
+                # need to sort before mining.
+                tree.sort()
+                frequency = tree.item_count.copy()
+            assert(tree.num_transactions == len(sliding_window))
+            assert(len(sliding_window) == window_size)
+            min_count = min_support * tree.num_transactions
+            patterns = fp_growth(tree, min_count, [])
+            yield (num_transactions - len(sliding_window), len(sliding_window), patterns)
+    else:
+        # We didn't just mine on the last transaction, we need to mine now,
+        # else we'll miss data.
+        if (num_transactions % window_size) != 0:
+            if (num_transactions % sort_interval) != 0:
+                tree.sort()
+                frequency = tree.item_count.copy()
+            min_count = min_support * tree.num_transactions
+            patterns = fp_growth(tree, min_count, [])
+            yield (num_transactions - len(sliding_window), len(sliding_window), patterns)
--- test_fptree.py
+++ test_fptree.py
@@ -1,7 +1,6 @@
 from fptree import FPTree
 from fptree import mine_fp_tree
 from fptree import construct_initial_tree
-from fptree import mine_cp_tree_stream
 from apriori import apriori
 from index import InvertedIndex
 from item import Item
@@ -9,38 +8,6 @@ from item import ItemSet
 import time
 import csv
 import sys
-
-
-def test_cp_tree_stream():
-    # (csvFilePath, min_support, sort_interval, window_size)
-    datasets = [
-        ("datasets/UCI-zoo.csv", 0.3, 10, 20),
-        ("datasets/mushroom.csv", 0.4, 500, 500),
-        # ("datasets/BMS-POS.csv", 0.05, 50000, 50000),
-    ]
-    for (csvFilePath, min_support, sort_interval, window_size) in datasets:
-        with open(csvFilePath, newline='') as csvfile:
-            print("test_cp_tree_stream {}".format(csvFilePath))
-            transactions = list(csv.reader(csvfile))
-            print("Loaded data file, {} lines".format(len(transactions)))
-            for (
-                    window_start_index,
-                    window_length,
-                    cptree_itemsets) in mine_cp_tree_stream(
-                    transactions,
-                    min_support,
-                    sort_interval,
-                    window_size):
-                print("Window {} + {} / {}".format(window_start_index,
-                                                   window_size, len(transactions)))
-                window = transactions[window_start_index:
-                                      window_start_index + window_length]
-                fptree_itemsets = mine_fp_tree(window, min_support)
-                print(
-                    "fptree produced {} itemsets, cptree produced {} itemsets".format(
-                        len(fptree_itemsets),
-                        len(cptree_itemsets)))
-                assert(set(cptree_itemsets) == set(fptree_itemsets))
 
 
 def test_basic_sanity():
