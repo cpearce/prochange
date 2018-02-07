e429e7285b43e9fd83fa0300869660bdebcdcfc7
Chris Pearce <chris@pearce.org.nz>
2018-02-07 19:37:47 +1300

    Make rule tree optionally use a sliding window.

--- ruletree.py
+++ ruletree.py
@@ -1,5 +1,6 @@
 from item import Item
 from collections import Counter
+from collections import deque
 
 
 class RuleTreeNode:
@@ -16,27 +17,40 @@ class RuleTreeNode:
             self.antecedent_children[item] = RuleTreeNode()
         self.antecedent_children[item].insert(antecedent[1:], consequent)
 
-    def record_matches(self, itemset, path, match_counter):
+    def is_empty(self):
+        return len(
+            self.antecedent_children) == 0 and len(
+            self.consequent_children) == 0
+
+    def matches(self, itemset, path):
         if len(itemset) == 0:
-            return False
-        found_match = False
+            return
         for index in range(len(itemset)):
             item = itemset[index]
             if item in self.antecedent_children:
-                found_match |= self.antecedent_children[item].record_matches(
-                    itemset[index + 1:], path + [item], match_counter)
+                for match in self.antecedent_children[item].matches(
+                        itemset[index + 1:], path + [item]):
+                    yield match
             if item in self.consequent_children:
-                match_counter[(tuple(path), item)] += 1
-                found_match = True
-        return found_match
+                yield (tuple(path), item)
+
+    def rules(self, antecedent_path):
+        result = set()
+        for consequent in self.consequent_children:
+            result.add((tuple(antecedent_path), consequent))
+        for antecedent, child in self.antecedent_children.items():
+            result |= child.rules(antecedent_path + [antecedent])
+        return result
 
 
 class RuleTree:
-    def __init__(self):
+    def __init__(self, window_size=None):
         self.root = RuleTreeNode()
         self.match_counter = Counter()
         self.rag_bag_count = 0
         self.transaction_count = 0
+        self.window_size = window_size
+        self.window = deque()
 
     def insert(self, antecedent, consequent):
         if len(antecedent) == 0:
@@ -58,10 +72,30 @@ class RuleTree:
         if not all(map(lambda i: isinstance(i, Item), itemset)):
             raise TypeError("itemset should contain Items")
         itemset.sort()
-        found_match = self.root.record_matches(itemset, [], self.match_counter)
+        found_match = False
+        for (antecedent, consequent) in self.root.matches(itemset, []):
+            self.match_counter[antecedent, consequent] += 1
+            found_match = True
         if not found_match:
             self.rag_bag_count += 1
         self.transaction_count += 1
+        if self.window_size is not None:
+            self.window.append(itemset)
+            if len(self.window) > self.window_size:
+                itemset = self.window.popleft()
+                self.remove_matches(itemset)
+            assert(self.transaction_count == len(self.window))
+
+    def remove_matches(self, itemset):
+        if not all(map(lambda i: isinstance(i, Item), itemset)):
+            raise TypeError("itemset should contain Items")
+        found_match = False
+        for (antecedent, consequent) in self.root.matches(itemset, []):
+            self.match_counter[(antecedent, consequent)] -= 1
+            found_match = True
+        if not found_match:
+            self.rag_bag_count -= 1
+        self.transaction_count -= 1
 
     def rag_bag(self):
         return self.rag_bag_count / self.transaction_count
@@ -74,20 +108,39 @@ class RuleTree:
                                    key=lambda i: i[0]))))
 
     def rule_miss_rate(self):
+        # Can only take match counts if we're not using a sliding window.
+        assert(self.window_size is None)
         return ((self.transaction_count - self.rag_bag_count) /
                 self.transaction_count, self.transaction_count)
 
     def clear_rule_match_counts(self):
+        # Can only take match counts if we're not using a sliding window.
+        assert(self.window_size is None)
         self.match_counter.clear()
         self.transaction_count = 0
         self.rag_bag_count = 0
 
     def take_and_add_matches(self, other):
+        # Can only take match counts if we're not using a sliding window.
+        assert(self.window_size is None)
         self.match_counter.update(other.match_counter)
         self.transaction_count += other.transaction_count
         self.rag_bag_count += other.rag_bag_count
         other.clear_rule_match_counts()
 
     def take_and_overwrite_matches(self, other):
+        # Can only take match counts if we're not using a sliding window.
+        assert(self.window_size is None)
         self.clear_rule_match_counts()
         self.take_and_add_matches(other)
+
+    def match_count_of(self, antecedent, consequent):
+        if not isinstance(antecedent, tuple):
+            raise TypeError("antecedent should be a Tuple of Items")
+        if not isinstance(consequent, Item):
+            raise TypeError("consequent should be an Item")
+        return self.match_counter[(antecedent, consequent)]
+
+    def rules(self):
+        # Returns a set of (antecedent,consequent) pairs of rules in the tree.
+        return self.root.rules([])
--- test_ruletree.py
+++ test_ruletree.py
@@ -16,16 +16,24 @@ def test_basic_rule_tree():
     ]
     rules = map(lambda r: (ItemSet(r[0]), ItemSet(r[1])), rules)
 
-    tree = RuleTree()
+    tree = RuleTree(4)
     for (antecedent, consequent) in rules:
         tree.insert(antecedent, consequent)
 
     # (ItemSet, [(antecedent, consquent)...])
     test_cases = [
         ("abcd", [1, 0, 0, 1, 0]),
-        ("geabcd", [1, 0, 0.5, 1, 0.5])
+        ("geabcd", [1, 0, 0.5, 1, 0.5]),
+        ("abc", [2 / 3, 0.0, 1 / 3, 2 / 3, 1 / 3]),
+        ("bcd", [0.5, 0.0, 0.25, 0.75, 0.25]),
+        ("def", [0.2, 0.0, 0.2, 0.4, 0.2]),
+        ("geab", [0.0, 0.0, 0.0, 1 / 6, 0.0]),
     ]
     test_cases = list(map(lambda t: (ItemSet(t[0]), t[1]), test_cases))
+    print()
     for (itemset, expected_results) in test_cases:
+        print("Adding {}".format(itemset))
         tree.record_matches(itemset)
+        for (a, c) in tree.rules():
+            print("  {} -> {} ; {}".format(a, c, tree.match_count_of(a, c)))
         assert(expected_results == tree.match_vector())
