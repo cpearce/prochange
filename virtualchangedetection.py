6947a1ab633aa0401107594ed3423d54ba6c20e8
Chris Pearce <chris@pearce.org.nz>
2018-02-11 23:11:05 +1300

    Update comment in main describing how to run virtual change detection.

--- virtualchangedetection.py
+++ virtualchangedetection.py
@@ -1,9 +1,9 @@
 # Example usage:
 #   $ python3 virtual-change-dectection.py \
-#       --input datasets/poker-hand-training-true.data \
-#       --output poker.rules \
+#       --input datasets/T1M_DP_V10R20_13.csv \
+#       --output rules.csv \
 #       --min-confidence 0.05 \
-#       --min-support 0.05 \
+#       --min-support 0.001 \
 #       --min-lift 1.0 \
 #       --training-window-size 2500 \
 #       --drift-algorithm prochange
